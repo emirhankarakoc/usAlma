6b7f48dda6a9aab3f92264d8bccbb37a275d08a6
emirhankarakoc <emirhankarakoc@yahoo.com>
2023-01-06 23:46:31 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import static java.lang.Math.sqrt;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int taban = 2;
+       double us =8;
+
+int deger = 1;
+        for(int i= 0;i<us;i++){
+
+deger*=taban;
+
+
+
+        }
+        System.out.println(deger);
+    }
+}
